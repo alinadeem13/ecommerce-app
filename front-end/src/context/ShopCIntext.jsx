4a9b3f19d8976701c19749b3073e38a0e2ee6d40
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-11-21 16:37:11 +0500

    Preparing Collection Images

--- front-end/src/context/ShopCIntext.jsx
+++ front-end/src/context/ShopCIntext.jsx
@@ -1,15 +1,16 @@
 import { createContext } from "react";
 import { products } from "../assets/assets";
 
-export const ShopContext = createContext();
+export const ShopContext = createContext(null); // FIX 1: Add default value
 
-const shopContextProvider = (props) => {
+const ShopContextProvider = ({ children }) => {
+  // FIX 2: Destructure props
   const currency = "$";
   const delivery_fee = 10;
 
   const value = { products, currency, delivery_fee };
-  return (
-    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
-  );
+
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
-export default shopContextProvider;
+
+export default ShopContextProvider;
