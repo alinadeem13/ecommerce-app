b60010b3e700370743c79d43cd28603a2b8a284d
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-11-25 23:54:28 +0500

    Setting up Policies and NEwSletter section components

---
+++ front-end/src/components/NewsLetterBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NewsLetterBox = () => {
+  return (
+    <div className="text-center">
+      <p className="text-2xl font-medium text-gray-800">
+        Suscribe now & get 20% off.
+      </p>
+    </div>
+  );
+};
